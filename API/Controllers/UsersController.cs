16a3e4f30ca328a3c363202a5f1b2460e0c2ec55
MaikArnold <maik.arnold@web.de>
2021-02-06 21:16:26 +0100

    Wieder original - Alle Kommentare raus

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -14,19 +14,16 @@ namespace API.Controllers
         private readonly DataContext _context;
         public UsersController(DataContext context)
         {
-            // Zugriff auf die Datenbank
             _context = context;
         }
 
         [HttpGet]
-        // Asynchrone Version
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        // Asynchrone Version
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
