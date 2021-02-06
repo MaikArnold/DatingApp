e7382bd853a638650d23f7e94058ca496a063704
MaikArnold <maik.arnold@web.de>
2021-02-06 21:11:25 +0100

    Test

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -26,6 +26,7 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        // Asynchrone Version
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
